73cc7181eaf2e6ae4bbe233b41ff634063b72127
hotnidammanurung <megamanurung5@gmail.com>
2017-05-10 23:15:15 +0700

    Tugas ini dikerjakan untuk menyelesaikan tugas yang diberi oleh asslab kami Bang Ruswan. Dimana tugas ini merupakan pembuatan sebuah program aplikasi kalkulator dengan aplikasi Intelij. Kiranya program atau aplikasi kami ini berjalan dengan lancar. Terimakasih..

---
+++ src/KalkulatorFrame/KalkulatorFrame.java
@@ -0,0 +1,12 @@
+package KalkulatorFrame;
+
+/**
+ * Created by ASUS on 10/05/2017.
+ */
+public class KalkulatorFrame {
+    public void setDefaultCloseOperation(int exitOnClose) {
+    }
+
+    public void setVisible(boolean b) {
+    }
+}
---
+++ src/KalkulatorPanel/KalkulatorPanel.java
@@ -0,0 +1,7 @@
+package KalkulatorPanel;
+
+/**
+ * Created by ASUS on 10/05/2017.
+ */
+public class KalkulatorPanel {
+}
